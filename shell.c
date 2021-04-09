47fb33c143681ac797f4275e969bad8fade4d48e
Nayana Yeshlur <nyeshlur@uw.edu>
2021-04-09 08:34:23 -0700

    getting started

--- shell.c
+++ shell.c
@@ -6,6 +6,7 @@ gcc shell.c
 
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 #define MAX_LINE 80 /* The maximum length command */
 
@@ -17,7 +18,45 @@ int main(void)
   while (should_run) {
     printf("osh>");
     fflush(stdout);
+    
+    char theCommand[MAX_LINE];
+    fgets(theCommand, sizeof(theCommand), stdin);
 
+    char *word = strtok(theCommand, " ");
+    args[0] = word;
+    word = strtok(NULL, " ");
+    args[1] = word;
+    word = strtok(NULL, " ");
+    args[2] = word;
+
+    //printf("%s %s", args[0], args[1]);
+    char str1[] = "exit";
+    int compare = strcmp(args[0], exit);
+
+    if(compare == 0) {
+      printf("exit");
+    }
+
+/*
+    if(args[2] == "&") {
+      printf("&");
+    } else {
+      printf("not &");
+    }
+*/
+    //printf("%c\n", theCommand[0]);
+    should_run = 0;
+
+    
+    //int pid = fork();
+    //if (pid == 0) {
+        //execvp(char *command, char *params[])
+    //} else {
+        //if (command does not include &) {
+          //wait
+        //}
+    //}
+    
     /**
     * After reading user input, the steps are:
     * (1) fork a child process using fork()
